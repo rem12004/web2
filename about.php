0e4ca6c76ba50d2f6b4e627d8da60247b0be72ab
Chris Remillard <rem12004@byui.edu>
2016-05-08 22:07:06 -0500

    media

---
+++ about.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>CS_313 Homepage</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/index.css" rel="stylesheet">
+	
+  </head>
+
+  <body >
+
+    <div class="container">
+      <div class="header">
+        <ul class="nav nav-pills pull-right">
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="about.php">About Me</a></li>
+		   <li><a href="assignments.php">Assignments</a></li>
+          <li><a href="mailto:rem12004@byui.edu">Contact</a></li>
+        </ul>
+        <h3 class="text-muted">Homepage CS_313</h3>
+      </div>
+
+      <div class="headImage">
+	  
+	  <a href="https://www.youtube.com/watch?v=ukUBm7Lkw78"><img src="media/knight.JPG" alt="Knight" width= "300" height="400"></a>
+	  
+      </div>
+
+      <div class="row marketing">
+        <div class="col-lg-6">
+         
+           <h4>Personal Info:<h4/>
+			<p><i>Good day sir or madame; as the title suggests I am Sir Christopher of the Remillards</i></p>
+			<p><i>I hail from the humble province of Gulfport Mississippi and am in the pursuit of a squireship in the art of Computer Science</i></p>
+			<p><i>This quest has lead me to this great institution; the Brigham Young University of Idaho</i></p>
+			<p><i>At the end of my studies here I shall journey to a distant land in search of greener pastures</i></p>
+        </div>
+
+       
+      </div>
+
+      <div class="footer">
+        <p>&copy; CMR 2016</p>
+      </div>
+
+    </div> 
+	
+  </body>
+</html>
